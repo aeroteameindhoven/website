a70ec1a458de53f808c964e74efdc76fc55ebf3d
Daan <daanhegger@gmail.com>
2022-02-02 15:57:34 +0100

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,15 @@
 import * as React from "react";
+import styles from "./index.module.scss";
 
 const IndexPage = () => {
 
-  const buttons: number[] = [1,2,3,4,5,6,7];
-
   return (
     <main>
-      Hoi daar
-      {buttons.filter((a) => a % 2 ==0).map((a) => <Button key={a} text={`Knop ${a}`} />)}
+      
+      <div className={styles.header}></div>
       
     </main>
   );
-};
-
-interface ButtonProps {
-  /**
-   * This text is the label of the button
-   */
-  text: string;
-}
-
-/**
- * This button has red text and margin
- */
-const Button: React.FC<ButtonProps> = (props) => {
-  return <button style={{ color: "red", fontSize: 20, display: "block", marginBottom: 20}}>{props.text}</button>;
 };
 
 export default IndexPage;
