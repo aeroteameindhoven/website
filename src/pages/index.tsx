52bff739d71b615d448cfb9d55beb60cdc2d070b
Yzmoo <32572845+Yzmoo@users.noreply.github.com>
2022-02-08 18:20:30 +0100

    skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
-import { header, nav } from './index.module.scss';
+import { header, nav, logo } from './index.module.scss';
 
 
 const IndexPage = () => {
@@ -8,34 +9,38 @@ const IndexPage = () => {
   return (
     <div className = {header}>
       <nav>
+      <StaticImage className={logo}
+        alt = "placeholder logo"
+        src="../images/placeHolders/logo.png"
+        ></StaticImage>
         <ul className={nav}>
           <li className={nav}>
-            <Link to="/" className={nav}>
+            <Link to='/about' className={nav}>
               About Us
             </Link>
           </li>
           <li className={nav}>
-            <Link to="/" className={nav}>
+            <Link to="/project" className={nav}>
               Project
             </Link>
           </li>
           <li className={nav}>
-            <Link to="/" className={nav}>
+            <Link to="/journey" className={nav}>
               Our Journey
             </Link>
           </li>
           <li className={nav}>
-            <Link to="/" className={nav}>
+            <Link to="/partners" className={nav}>
               Partners
             </Link>
           </li>
           <li className={nav}>
-            <Link to="/about" className={nav}>
+            <Link to="/join" className={nav}>
               Join
             </Link>
           </li>
           <li className={nav}>
-            <Link to="/about" className={nav}>
+            <Link to="/contact" className={nav}>
               Contact
             </Link>
           </li>
@@ -43,6 +48,11 @@ const IndexPage = () => {
       </nav>
       <main>
         <h1>Working towards a sustainable future</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+        <StaticImage
+        alt = "placeholder globe"
+        src="../images/placeHolders/globe.png"
+        ></StaticImage>
       </main>
    </div>
 
