9b1bc7fd5df0421f7ff35098d11453de359ee327
Yzmoo <32572845+Yzmoo@users.noreply.github.com>
2022-05-09 22:43:31 +0200

    text showing when hovering

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -18,8 +18,15 @@ const Partners: React.FC = () => {
             <p>Helping us lift off!</p>
             <div>
               {partnerInfo.map((v) => {
-                const Logo = v.logo;
-                return Logo;
+                const Logo = <div className="partner-logo">{v.logo}</div>;
+                const Text = <p className="text">{v.text}</p>;
+                const Info = (
+                  <div className="partner-info">
+                    {Logo}
+                    {Text}
+                  </div>
+                );
+                return Info;
               })}
             </div>
             <p> Do you want to help us redefine flying? Feel free to contact us!</p>
@@ -38,29 +45,17 @@ export default Partners;
 const partnerInfo = [
   {
     name: "GLS",
-    logo: (
-      <div className="logo">
-        <GLSLogo height={600} width={600} />
-      </div>
-    ),
+    logo: <GLSLogo height={600} width={600} />,
     text: "GLS is one of the biggest players in the shipping industry. Aside from being a titan of industry, GLS aims to constantly evolve, innovate and grow. Topics such as sustainability cannot be overlooked and are actively considered and tackled in the company. To Aero, GLS is the main strategical sponsor, and supplies the funding, advice and platform for the initial use case of package-delivery. Our solution fits perfectly within the vision of GLS and by actively working together faster and better progress can be booked. To this end, strategic advice is given on how the technological development of Aero can be properly implemented in the world of shipping, and how developments can be steered towards the right direction."
   },
   {
     name: "Tu/E",
-    logo: (
-      <div className="logo">
-        <TUELogo height={400} width={400} />
-      </div>
-    ),
+    logo: <TUELogo height={400} width={400} />,
     text: ""
   },
   {
     name: "NXP",
-    logo: (
-      <div className="logo">
-        <NXPLogo height={300} width={300} />
-      </div>
-    ),
+    logo: <NXPLogo height={300} width={300} />,
     text: "Across a broad spectrum of applications, NXP aims to advance our world by developing and supporting technological innovations. Their state-of-the-art components and extensive expertise allow them to actively compete in the world of technical parts, software and components. With a mindset of going towards a sustainable world they are one of the frontrunners in supporting sustainable innovation. NXP supports Aero in twofold by supplying both technical components for prototyping and product development, as well as professional technical support from experienced engineers."
   }
 ];
