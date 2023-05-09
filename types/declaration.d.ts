0dc7155ff84894a478cc196b66f83856b3fc0ff2
Bianca Eni <b.eni@aeroteameindhoven.nl>
2023-05-09 23:13:31 +0200

    remove npm garbage, remove leftover dependencies and comment declaration file
    Co-Authored-By: @DusterTheFirst

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -1,6 +1,8 @@
-// Declare scss files so they can be read by tsx
+// Declare bundler files so they can be read by tsx
 declare module "*.scss";
 declare module "*.svg";
-declare module "3d-react-carousal";
 declare module "*.jpg";
 declare module "*.png";
+
+// No types :(
+declare module "3d-react-carousal";
