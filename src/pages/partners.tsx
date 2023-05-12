cbdc9eabc0de9f91aabe8620ab69be038e3c7cf6
Bianca Eni <b.eni@aeroteameindhoven.nl>
2023-05-12 15:08:06 +0200

    all logos work now

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -89,7 +89,7 @@ const partnerInfo: Record<"platinum" | "gold" | "silver", PartnerInfo[]> = {
   gold: [
     {
       name: "NXP",
-      logo: <NXPLogo />,
+      logo: <NXPLogo className="nxp-logo" />,
       url: "https://www.nxp.com/",
       text: "Across a broad spectrum of applications, NXP aims to advance our world by developing and supporting technological innovations. Their state-of-the-art components and extensive expertise allow them to actively compete in the world of technical parts, software and components. With a mindset of going towards a sustainable world they are one of the frontrunners in supporting sustainable innovation. NXP supports Aero in twofold by supplying both technical components for prototyping and product development, as well as professional technical support from experienced engineers."
     }
