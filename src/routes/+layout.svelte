01633d38c45eba964bed7d22f1e5490815d3c799
Zachary Kohnen <me@dusterthefirst.com>
2023-11-27 14:06:25 +0100

    Very very small scaffolding

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+	<a href="/">Aero Team Eindhoven</a>
+	<a href="/team">Team</a>
+	<a href="/project">Project</a>
+	<a href="/partners">Partners</a>
+	<a href="/join">Join</a>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Redefine flying.</h2>
+
+<p>
+	We are working towards the world 's first aerial battery swap. Discover how we are reshaping the
+	future of aviation through cutting-edge power solutions.
+</p>
+<p>Join us in exploring the endless possibilities of flight!</p>
+<p>Official student team of TU/e (LOGO)</p>
