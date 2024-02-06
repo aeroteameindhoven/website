c5fb52880352b7aab9706d904006e111e2fc398b
Zachary Kohnen <me@dusterthefirst.com>
2024-02-06 16:52:55 +0100

    Use markdown files for the partners' definitions

---
+++ src/hooks/usePartners.ts
@@ -0,0 +1,97 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+const QUERY = graphql`
+  {
+    sponsors: allFile(
+      filter: { sourceInstanceName: { eq: "sponsors" } }
+      sort: { childMarkdownRemark: { frontmatter: { name: ASC } } }
+    ) {
+      group(field: { relativeDirectory: SELECT }) {
+        partner_package: fieldValue
+        nodes {
+          markdown: childMarkdownRemark {
+            html
+            frontmatter {
+              name
+              logo
+              url
+            }
+          }
+        }
+      }
+    }
+    images: allFile(filter: { sourceInstanceName: { eq: "sponsors-images" } }) {
+      nodes {
+        relativePath
+        publicURL
+      }
+    }
+  }
+`;
+
+interface QueryResult {
+  sponsors: {
+    group: {
+      partner_package: string;
+      nodes: {
+        markdown: {
+          html: string;
+          frontmatter: {
+            name: string;
+            logo: string;
+            url: string;
+          };
+        };
+      }[];
+    }[];
+  };
+  images: {
+    nodes: {
+      relativePath: string;
+      publicURL: string;
+    }[];
+  };
+}
+
+export const partner_packages = ["platinum", "gold", "silver"] as const;
+export type PartnerPackage = (typeof partner_packages)[number];
+export function isPartnerPackage(package_type: string): package_type is PartnerPackage {
+  return partner_packages.includes(package_type as PartnerPackage);
+}
+
+interface PartnerInfo {
+  name: string;
+  logo: string;
+  url: string;
+  html: string;
+}
+
+export function usePartners(): Map<PartnerPackage, PartnerInfo[]> {
+  const query = useStaticQuery<QueryResult>(QUERY);
+
+  return new Map(
+    query.sponsors.group.map((g) => {
+      if (!isPartnerPackage(g.partner_package)) {
+        throw new Error(`Invalid package in sponsor definitions: ${g.partner_package}`);
+      }
+
+      return [
+        g.partner_package,
+        g.nodes.map<PartnerInfo>(({ markdown }) => {
+          const logo = query.images.nodes.find((img) => img.relativePath === markdown.frontmatter.logo)?.publicURL;
+
+          if (logo === undefined) {
+            throw new Error(`Unable to find image '${markdown.frontmatter.logo}' for ${markdown.frontmatter.name}`);
+          }
+
+          return {
+            name: markdown.frontmatter.name,
+            logo,
+            url: markdown.frontmatter.url,
+            html: markdown.html
+          };
+        })
+      ];
+    })
+  );
+}
---
+++ src/sponsors/gold/HTC.md
@@ -0,0 +1,11 @@
+---
+name: HTCE UAM Hub
+logo: uam.svg
+url: "https://www.hightechcampus.com/uamhub"
+---
+
+Our collaboration with the Urban Air Mobility Hub at the High Tech Campus in
+Eindhoven provides a perfect urban testing environment for our drones. With
+controlled access and guidance on certifications, we can optimize our drone
+technologies for urban scenarios and contribute to the advancement of urban
+air mobility.
