94f632703a0c0e07bdcdbe0f0d66098222d4e7cf
Bianca Eni <b.eni@aeroteameindhoven.nl>
2024-03-30 13:21:54 +0100

    tried adding background and failed

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,22 +1,22 @@
 // import { Link } from "gatsby";
 import React from "react";
-import Layout from "../components/Layout";
+// import Layout from "../components/Layout";
 import "../components/styles/project.scss";
 import { HeadContent } from "../components/HeadContent";
 import ProjectCarousel from "../components/ProjectCarousel";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 export function Head() {
   return <HeadContent title="Project" />;
 }
 export default function Project() {
   return (
-<div className="projects-page">      
-      <Layout>
-        <div >
+    <div className="projects-page">
+      <Header></Header>
         <h1 className="projects-title">Projects</h1>
         <p className="projects-description">Want to know more about what we&apos;ve been working on?</p>
-        </div>
         <ProjectCarousel />
-      </Layout>
+        <Footer></Footer>
     </div>
   );
 }
