213a16431382e28914ac62bc6c9ea3d2be0b7f29
Bianca Eni <b.eni@aeroteameindhoven.nl>
2024-03-30 20:33:23 +0100

    gatsbyimages stuff

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,16 +1,17 @@
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 import "../components/styles/imagegallery.scss";
-import React from 'react';
+import React from "react";
 
 interface GalleryProps {
-  images: string[];
+  images: IGatsbyImageData[];
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images }) => {
   return (
-    <div className="gallery" data-aos="fade-up">
+    <div className="gallery">
       <div>Gallery</div>
-      {images.map((image: string, index: number) => (
-        <img key={index} src={image} alt={`Drone View ${index}`} className="gallery-image" />
+      {images.map((image, index) => (
+        <GatsbyImage key={index} image={image} alt={`Drone View ${index}`} className="gallery-image" />
       ))}
     </div>
   );
--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "../components/styles/projectdescription.scss";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 
 interface ProjectDescriptionProps {
   title: string;
   description: string;
-  imageSrc: string;
+  imageSrc: IGatsbyImageData;
 }
 
 const ProjectDescription: React.FC<ProjectDescriptionProps> = ({ title, description, imageSrc }) => {
   return (
-    <div className="project-description-section" data-aos="fade-up" data-aos-duration="2000">
+    <div className="project-description-section">
       <div className="text-container">
         <h2>{title}</h2>
         <div className="description">{description}</div>
       </div>
       <div className="image-container">
-      <StaticImage
-        src={imageSrc}
-        alt="Aero team member performing management tasks"
-        objectFit="cover"
-        className="image"
-      />
+        <GatsbyImage
+          image={imageSrc}
+          alt="Aero team member performing management tasks"
+          objectFit="cover"
+          className="image"
+        />
       </div>
     </div>
   );
