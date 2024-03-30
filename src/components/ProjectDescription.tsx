eb149cb2a4df3fcd29419c9fc52dfbf233b844e3
Bianca Eni <b.eni@aeroteameindhoven.nl>
2024-03-30 15:45:50 +0100

    added photo, generalized project description section

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -1,28 +1,24 @@
+import React from "react";
 import "../components/styles/projectdescription.scss";
 
-import React from "react";
+interface ProjectDescriptionProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+}
 
-const StatsSection = () => {
+const ProjectDescription: React.FC<ProjectDescriptionProps> = ({ title, description, imageSrc }) => {
   return (
     <div className="project-description-section" data-aos="fade-up" data-aos-duration="2000">
-      <h2>Aegle v2</h2>
-      <div className="description">
-        {" "}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod, ligula eu aliquam maximus, magna mi
-        suscipit libero, eget pretium felis mauris non ligula. Nam eu lectus egestas, suscipit tortor ac, aliquet elit.
-        Sed turpis arcu, scelerisque sed sapien sit amet, sollicitudin efficitur sapien. Sed volutpat dui eros, in
-        semper sapien maximus ac. Duis quis massa ac diam dapibus imperdiet. Vestibulum mattis euismod purus. Praesent
-        sed volutpat ante. Nam interdum lobortis leo, a auctor nisl fermentum sed. Maecenas quam metus, interdum id enim
-        vitae, imperdiet rhoncus mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra, erat at
-        finibus sollicitudin, orci turpis egestas neque, sit amet convallis massa neque eget leo. Aenean ac ipsum
-        auctor, egestas elit ut, semper nisl. Donec mollis, ligula blandit blandit sollicitudin, lectus dolor lacinia
-        mi, vel auctor ante massa quis quam. Praesent laoreet purus sit amet sagittis commodo. Cras facilisis urna
-        nulla, venenatis rutrum leo faucibus quis. Praesent vestibulum molestie neque sit amet malesuada. Praesent sem
-        mi, ullamcorper non orci nec, aliquam maximus elit. 
+      <div className="text-container">
+        <h2>{title}</h2>
+        <div className="description">{description}</div>
       </div>
-      <div></div>
+      <div className="image-container">
+        <img src={imageSrc} />
+      </div>
     </div>
   );
 };
 
-export default StatsSection;
+export default ProjectDescription;
--- src/pages/aegle2.tsx
+++ src/pages/aegle2.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import Aegle3D from '../components/3DAegle2'; 
-import ProjectDescription from '../components/ProjectDescription'; 
-import StatsSection from '../components/StatsSection';
-import Gallery from '../components/ImageGallery';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Aegle3D from "../components/3DAegle2";
+import ProjectDescription from "../components/ProjectDescription";
+import StatsSection from "../components/StatsSection";
+import Gallery from "../components/ImageGallery";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "../components/styles/imagegallery.scss";
 
 function Aegle2() {
@@ -14,13 +14,23 @@ function Aegle2() {
     AOS.init({ duration: 2000 });
   }, []);
 
-  const images: string[] = ["../images/project/Aegle.png", "../images/project/Spaerow.png"]; 
+  const images: string[] = ["../images/project/Aegle.png", "../images/project/Spaerow.png"];
+  const project = {
+    title: "Aeglev2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod, ligula eu aliquam maximus, magna mi suscipit libero, eget pretium felis mauris non ligula. Nam eu lectus egestas, suscipit tortor ac, aliquet elit. Sed turpis arcu, scelerisque sed sapien sit amet, sollicitudin efficitur sapien. Sed volutpat dui eros, in semper sapien maximus ac. Duis quis massa ac diam dapibus imperdiet. Vestibulum mattis euismod purus. Praesent sed volutpat ante. Nam interdum lobortis leo, a auctor nisl fermentum sed. Maecenas quam metus, interdum id enim vitae, imperdiet rhoncus mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra, erat at finibus sollicitudin, orci turpis egestas neque, sit amet convallis massa neque eget leo. Aenean ac ipsum auctor, egestas elit ut, semper nisl. Donec mollis, ligula blandit blandit sollicitudin, lectus dolor lacinia mi, vel auctor ante massa quis quam. ",
+    imageSrc: "../images/project/Aegle.png"
+  };
 
   return (
     <div className="aegle2">
       <Header></Header>
       <Aegle3D />
-      <ProjectDescription></ProjectDescription>
+      <ProjectDescription
+        title={project.title}
+        description={project.description}
+        imageSrc={project.imageSrc}
+      ></ProjectDescription>
       <StatsSection />
       <Gallery images={images} />
       <Footer></Footer>
