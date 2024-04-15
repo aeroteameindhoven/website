c404c4a5efc027f1579b5580fa04d6aa5b5187ba
Zachary Kohnen <me@dusterthefirst.com>
2024-04-15 13:52:55 +0200

    Setup partners page for success

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Container } from "react-grid-system";
 import Layout from "../components/Layout";
-import "../components/styles/partners.scss";
-
+import {
+  partnerInfo,
+  partnerLogo,
+  text,
+  partnerPage,
+  partnerHeader,
+  buttonWrapper,
+  contactButton
+} from "./partners.module.scss"; // TODO: https://www.npmjs.com/package/typed-scss-modules
 import { PartnersEmail } from "../components/Email";
 import { HeadContent } from "../components/HeadContent";
 import { PartnerPackage, partner_packages, usePartners } from "../queries/partners";
@@ -15,13 +22,13 @@ function PostHeader() {
   const partners = usePartners();
 
   return (
-    <div className="partner-page">
+    <div className={partnerPage}>
       <Container>
-        <div className="partner-header">
+        <div className={partnerHeader}>
           <h1>PARTNERS</h1>
           <p>Helping us lift off! Do you want to help us redefine flying? Feel free to contact us!</p>
-          <div className="button-wrapper">
-            <PartnersEmail className="contact-button">Contact Us!</PartnersEmail>
+          <div className={buttonWrapper}>
+            <PartnersEmail className={contactButton}>Contact Us!</PartnersEmail>
           </div>
         </div>
 
@@ -30,11 +37,11 @@ function PostHeader() {
             <>
               <h1>{package_name.toLocaleUpperCase()}</h1>
               {partners.get(package_name)?.map((v) => (
-                <div key={v.name} className="partner-info">
-                  <a href={v.url} className="partner-logo" target="_blank" rel="noreferrer">
+                <div key={v.name} className={partnerInfo}>
+                  <a href={v.url} className={partnerLogo} target="_blank" rel="noreferrer">
                     <img src={v.logo} />
                   </a>
-                  <div className="text" dangerouslySetInnerHTML={{ __html: v.html }} />
+                  <div className={text} dangerouslySetInnerHTML={{ __html: v.html }} />
                 </div>
               ))}
             </>
