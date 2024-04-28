e95d3a27fce524f093cc3b3bcdabb5ca31a500d4
Zachary Kohnen <me@dusterthefirst.com>
2024-04-28 17:05:08 +0200

    more template

--- static/admin/template.js
+++ static/admin/template.js
@@ -1,17 +1,25 @@
 CMS.registerPreviewTemplate("teams", ({ entry, widgetFor, widgetsFor }) => {
-  return h("div", {}, [
+  return h(
+    "div",
+    {},
     widgetFor("year"),
     widgetFor("description"),
-    widgetsFor("members").map((member, index) => {
-      return h("div", { key: index }, [
-        h("pre", {}, `${member.getIn(["data", "first_name"])} ${member.getIn(["data", "last_name"])}`),
-        h("pre", {}, member.getIn(["data", "function"])),
-        h("pre", {}, member.getIn(["data", "subteams"]).join(", ")),
-        h("pre", {}, member.getIn(["data", "email"])),
-        member.getIn(["widgets", "photo"]),
-        h("pre", {}, JSON.stringify(member))
-      ]);
-    }),
+    h(
+      "div",
+      {},
+      widgetsFor("members").map((member, index) => {
+        return h(
+          "div",
+          { key: index /*,  style: { display: "flex", flexDirection: "row" } */ },
+          h("pre", {}, `${member.getIn(["data", "first_name"])} ${member.getIn(["data", "last_name"])}`),
+          h("pre", {}, member.getIn(["data", "function"])),
+          h("pre", {}, member.getIn(["data", "subteams"]).join(", ")),
+          h("pre", {}, member.getIn(["data", "email"])),
+          h("div", { style: { maxHeight: "10em" } }, member.getIn(["widgets", "photo"])),
+          h("pre", {}, JSON.stringify(member))
+        );
+      })
+    ),
     h("pre", {}, JSON.stringify(entry.getIn(["data", "members"]), undefined, 4))
-  ]);
+  );
 });
