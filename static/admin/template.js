1e233bda6759c316ed61609c182d3c0fbce98e3f
Zachary Kohnen <me@dusterthefirst.com>
2024-04-29 12:00:33 +0200

    Finish template

--- static/admin/template.js
+++ static/admin/template.js
@@ -1,4 +1,6 @@
-CMS.registerPreviewTemplate("teams", ({ entry, widgetFor, widgetsFor }) => {
+// CMS.registerPreviewStyle("preview.css");
+
+CMS.registerPreviewTemplate("teams", ({ widgetFor, widgetsFor, getAsset }) => {
   return h(
     "div",
     {},
@@ -8,18 +10,28 @@ CMS.registerPreviewTemplate("teams", ({ entry, widgetFor, widgetsFor }) => {
       "div",
       {},
       widgetsFor("members").map((member, index) => {
+        console.dir(member.toJS());
+
         return h(
           "div",
-          { key: index /*,  style: { display: "flex", flexDirection: "row" } */ },
-          h("pre", {}, `${member.getIn(["data", "first_name"])} ${member.getIn(["data", "last_name"])}`),
-          h("pre", {}, member.getIn(["data", "function"])),
-          h("pre", {}, member.getIn(["data", "subteams"]).join(", ")),
-          h("pre", {}, member.getIn(["data", "email"])),
-          h("div", { style: { maxHeight: "10em" } }, member.getIn(["widgets", "photo"])),
-          h("pre", {}, JSON.stringify(member))
+          { key: index, style: { display: "flex", flexDirection: "row", marginBlock: "1em" } },
+          h("img", {
+            style: { maxHeight: "10em", marginInlineEnd: "1em", backgroundColor: "#808080", aspectRatio: "2/3" },
+            src: getAsset(member.getIn(["data", "photo"])).toString() // TODO: Placeholder image
+          }),
+          h(
+            "div",
+            { style: { marginBlock: "auto" } },
+            `${member.getIn(["data", "first_name"]) ?? ""} ${member.getIn(["data", "last_name"]) ?? ""}`,
+            h("br"),
+            member.getIn(["data", "function"]) ?? "",
+            h("br"),
+            (member.getIn(["data", "subteams"]) ?? []).join(" + "),
+            h("br"),
+            member.getIn(["data", "email"]) ?? ""
+          )
         );
       })
-    ),
-    h("pre", {}, JSON.stringify(entry.getIn(["data", "members"]), undefined, 4))
+    )
   );
 });
