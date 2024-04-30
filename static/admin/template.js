1312c49539c329ae818a525e9067e6203d517a22
Zachary Kohnen <me@dusterthefirst.com>
2024-04-30 16:15:57 +0200

    Filter out when members is non array

--- static/admin/template.js
+++ static/admin/template.js
@@ -1,6 +1,8 @@
 // CMS.registerPreviewStyle("preview.css");
 
 CMS.registerPreviewTemplate("teams", ({ widgetFor, widgetsFor, getAsset }) => {
+  const members_widgets = widgetsFor("members").toJS();
+
   return h(
     "div",
     {},
@@ -9,29 +11,28 @@ CMS.registerPreviewTemplate("teams", ({ widgetFor, widgetsFor, getAsset }) => {
     h(
       "div",
       {},
-      widgetsFor("members").map((member, index) => {
-        console.dir(member.toJS());
-
-        return h(
-          "div",
-          { key: index, style: { display: "flex", flexDirection: "row", marginBlock: "1em" } },
-          h("img", {
-            style: { maxHeight: "10em", marginInlineEnd: "1em", backgroundColor: "#808080", aspectRatio: "2/3" },
-            src: getAsset(member.getIn(["data", "photo"])).toString() // TODO: Placeholder image
-          }),
-          h(
+      Array.isArray(members_widgets) &&
+        widgetsFor("members").map((member, index) => {
+          return h(
             "div",
-            { style: { marginBlock: "auto" } },
-            `${member.getIn(["data", "first_name"]) ?? ""} ${member.getIn(["data", "last_name"]) ?? ""}`,
-            h("br"),
-            member.getIn(["data", "function"]) ?? "",
-            h("br"),
-            (member.getIn(["data", "subteams"]) ?? []).join(" + "),
-            h("br"),
-            member.getIn(["data", "email"]) ?? ""
-          )
-        );
-      })
+            { key: index, style: { display: "flex", flexDirection: "row", marginBlock: "1em" } },
+            h("img", {
+              style: { maxHeight: "10em", marginInlineEnd: "1em", backgroundColor: "#808080", aspectRatio: "2/3" },
+              src: getAsset(member.getIn(["data", "photo"])).toString() // TODO: Placeholder image
+            }),
+            h(
+              "div",
+              { style: { marginBlock: "auto" } },
+              `${member.getIn(["data", "first_name"]) ?? ""} ${member.getIn(["data", "last_name"]) ?? ""}`,
+              h("br"),
+              member.getIn(["data", "function"]) ?? "",
+              h("br"),
+              (member.getIn(["data", "subteams"]) ?? []).join(" + "),
+              h("br"),
+              member.getIn(["data", "email"]) ?? ""
+            )
+          );
+        })
     )
   );
 });
