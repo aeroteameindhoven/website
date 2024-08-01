2eb08e09d129d6098f2edb52a2a792bbdc6e75db
Zachary Kohnen <z.kohnen@aeroteameindhoven.nl>
2024-08-01 02:14:57 -0400

    Revamp the sponsors page (#80)
    * Setup partners page for success
    
    * Fix infinite logo
    
    * renovate the sponsors listing

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Container } from "react-grid-system";
 import Layout from "../components/Layout";
-import "../components/styles/partners.scss";
-
+import {
+  partnerInfo,
+  partnerLogo,
+  text,
+  partnerPage,
+  partnerHeader,
+  buttonWrapper,
+  contactButton,
+  partnerGroup,
+  details
+} from "./partners.module.scss"; // TODO: https://www.npmjs.com/package/typed-scss-modules
 import { PartnersEmail } from "../components/Email";
 import { HeadContent } from "../components/HeadContent";
 import { PartnerPackage, partner_packages, usePartners } from "../queries/partners";
@@ -15,13 +24,13 @@ function PostHeader() {
   const partners = usePartners();
 
   return (
-    <div className="partner-page">
+    <div className={partnerPage}>
       <Container>
-        <div className="partner-header">
+        <div className={partnerHeader}>
           <h1>PARTNERS</h1>
           <p>Helping us lift off! Do you want to help us redefine flying? Feel free to contact us!</p>
-          <div className="button-wrapper">
-            <PartnersEmail className="contact-button">Contact Us!</PartnersEmail>
+          <div className={buttonWrapper}>
+            <PartnersEmail className={contactButton}>Contact Us!</PartnersEmail>
           </div>
         </div>
 
@@ -29,14 +38,19 @@ function PostHeader() {
           return (
             <>
               <h1>{package_name.toLocaleUpperCase()}</h1>
-              {partners.get(package_name)?.map((v) => (
-                <div key={v.name} className="partner-info">
-                  <a href={v.url} className="partner-logo" target="_blank" rel="noreferrer">
-                    <img src={v.logo} />
-                  </a>
-                  <div className="text" dangerouslySetInnerHTML={{ __html: v.html }} />
-                </div>
-              ))}
+              <div className={partnerGroup}>
+                {partners.get(package_name)?.map((v) => (
+                  <div key={v.name} className={partnerInfo}>
+                    <a href={v.url} className={partnerLogo} target="_blank" rel="noreferrer">
+                      <img src={v.logo} />
+                    </a>
+                    <details className={details}>
+                      <summary>Learn More...</summary>
+                      <div className={text} dangerouslySetInnerHTML={{ __html: v.html }} />
+                    </details>
+                  </div>
+                ))}
+              </div>
             </>
           );
         })}
